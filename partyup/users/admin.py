81239f594aef31a770b4e2d20e4be238860906b3
David Gilhooley <djg5@princeton.edu>
2015-04-05 23:38:30 +0000

    Created the admin page

--- partyup/users/admin.py
+++ partyup/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User_Profile, Event, Group
 
+admin.site.register(User_Profile)
+admin.site.register(Event)
+admin.site.register(Group)
 # Register your models here.
