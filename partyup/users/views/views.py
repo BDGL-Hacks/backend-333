f598c3a1a917a43f3d6ca33f00999a9721f4904f
David Gilhooley <djg5@princeton.edu>
2015-04-10 03:10:45 +0000

    Moved user functions to a the accounts.py view file. Added location_name parameter to events/create

--- partyup/users/views/views.py
+++ partyup/users/views/views.py
@@ -5,43 +5,6 @@ from django.contrib.auth import login, authenticate
 from users.models import User_Profile, Event, Group
 
 
-@csrf_exempt
-def register(request):
-    response = {}
-    if request.method == 'POST':
-        user_data = request.POST
-
-        user_email = user_data.get('email', '')
-        user_fn = user_data.get('first_name', '')
-        user_ln = user_data.get('last_name', '')
-        user_pswd = user_data.get('password', '')
-
-        # Ensure all parameters were included in the request
-        if not user_email or not user_fn or not user_ln or not user_pswd:
-            response['error'] = "MISSING INFO"
-            response['accepted'] = False
-            return JsonResponse(response)
-
-        # checks if email is used
-        if User.objects.filter(email=user_email):
-            response['error'] = 'That email is being used. Please use another email'
-            response['accepted'] = False
-            return JsonResponse(response)
-
-        # Create the new account and a profile for that account
-        # TODO: consider sanitizing input
-        user = User.objects.create_user(username=user_email, password=user_pswd,
-                                        email=user_email, first_name=user_fn,
-                                        last_name=user_ln)
-
-        # Add new user to the database
-        User_Profile(user=user).save()
-        response['accepted'] = True
-        return JsonResponse(response)
-    else:
-        response['error'] = 'NOT A POST REQUEST'
-        response['accepted'] = False
-        return JsonResponse(response)
 
 
 @csrf_exempt
@@ -128,42 +91,3 @@ def create_group(request):
     return JsonResponse(response)
 
 
-@csrf_exempt
-def login_view(request):
-    response = {}
-    if request.user.is_authenticated():
-        response['error'] = 'You are already logged in'
-        response['accepted'] = False
-        return JsonResponse(response)
-
-    if request.method == 'POST':
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        if not username or not password:
-            response['error'] = 'MISSING INFO'
-            response['accepted'] = False
-            return JsonResponse(response)
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                response['accepted'] = True
-                return JsonResponse(response)
-            else:
-                response['error'] = 'Your account has been disabled'
-                response['accepted'] = False
-                return JsonResponse(response)
-        else:
-            response['error'] = 'Wrong Username or Password'
-            response['accepted'] = False
-            return JsonResponse(response)
-    else:
-        response['error'] = 'NOT A POST REQUEST'
-        response['accepted'] = False
-        return JsonResponse(response)
-
-
-@csrf_exempt
-def user_search(request):
-    pass
