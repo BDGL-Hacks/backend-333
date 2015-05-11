aa9cbee57e20f90d67f2ea1f60bbc37379e6c695
David Gilhooley <djg5@princeton.edu>
2015-05-11 15:03:26 -0400

    commented all of the model objects

--- partyup/users/models.py
+++ partyup/users/models.py
@@ -6,6 +6,13 @@ from push_notifications.models import APNSDevice
 
 # Additional information about users that is not needed at registration
 class User_Profile(models.Model):
+    '''
+    This is the model for a single PartyUp user. The user has relevant information such as
+    birthday, events attending and invited, groups attending and invited, a profile picture, 
+    and an iOS device.
+
+    A user's username, password, and name are stored in the Django's built-in user
+    '''
     # Built-in Django user
     # See https://docs.djangoproject.com/en/1.7/ref/contrib/auth/
     # for information about models.User
@@ -54,10 +61,15 @@ class User_Profile(models.Model):
 
 
 class User_Group_info(models.Model):
+    '''
+    This model stores information relating a single user and a single group.
+    It will keep information such as the user's indicator status (red, green, yellow),
+    and the last event the user was at for the group
+    '''
     # The current location of a user
     status = models.ForeignKey('Event')
 
-    # The user's status (can be 0, 1, or 2)
+    # The user's status (can be 0=green, 1=yellow, or 2=red)
     indicator = models.IntegerField(default=0)
 
     user_profile = models.ForeignKey('User_Profile')
@@ -67,6 +79,10 @@ class User_Group_info(models.Model):
         return '%s %s' % (self.user_profile, self.group)
 
 class Event(models.Model):
+    '''
+    The model for a single event. Contains personal fields such as date created, time of event, event admin, title, location, price, etc.
+    It contains relational information such as who is attending or invited to the event
+    '''
     date_created = models.DateTimeField('date published', default=datetime.now)
     created_by = models.ForeignKey(User_Profile, related_name='event_creator')
 
@@ -108,7 +124,10 @@ class Event(models.Model):
             'id': self.id,
             'picture': self.picture,
         }
-
+    
+    # A shorter dictionary that is used when sending back multiple events
+    # The client is expected to query for an event that they would like 
+    # more information on
     def to_dict_sparse(self):
         return {
             'admin': self.admin.to_dict(),
@@ -120,6 +139,11 @@ class Event(models.Model):
 
 
 class Ping(models.Model):
+    '''
+    A single ping sent to a user within a Group.
+    The ping has information of how it was responded to, the time it was
+    sent, and the user and group it is associated with
+    '''
     # The user the ping was sent to
     user = models.ForeignKey(User_Profile)
 
@@ -129,6 +153,8 @@ class Ping(models.Model):
     # When the ping was sent
     time = models.DateTimeField(default=datetime.now)
 
+    # Return a dictionary of the ping's information
+    # Can be turned into json to send in an API call
     def to_dict(self):
         return {
             'user': self.user.to_dict(),
@@ -138,6 +164,13 @@ class Ping(models.Model):
 
 
 class Group(models.Model):
+    '''
+    A single Group object. Contains personal information such as group title,
+    the date created, and if the group is active.
+    Contains relation information such as who is attending or invited,
+    the pings that were sent in the group, and what events the 
+    group is attending
+    '''
     title = models.CharField(max_length=100)
     date_created = models.DateTimeField('date published', default=datetime.now)
     created_by = models.ForeignKey(User_Profile, related_name='group_creator')
@@ -157,6 +190,8 @@ class Group(models.Model):
     def __str__(self):
         return '%d %s' % (self.id, self.title)
 
+    # sends back a dict that contains all group information.
+    # This dict can be turned into JSON for the API
     def to_dict(self):
         members = []
         for member in self.group_members.all():
@@ -190,8 +225,13 @@ class Group(models.Model):
 
 class Channel(models.Model):
     '''
-    Pusher channel
+    This holds information for a Pusher Channel for a single group.
+    It keeps track of the group, the channel name, and the number
+    of messages sent
     '''
+    # The channel name that the group is listening to on Pusher
+    # It is currently 'GroupChat' + the id of the group
+    # eg: GroupChat11
     name = models.CharField(max_length=50, null=True)
     group = models.ForeignKey(Group)
     num_messages = models.IntegerField(default=0)
@@ -199,7 +239,7 @@ class Channel(models.Model):
 
 class Message(models.Model):
     '''
-    Message in a group chat
+    A single Message in a group chat
     '''
     channel = models.ForeignKey(Channel)
     time_sent = models.DateTimeField('date published', default=datetime.now)
